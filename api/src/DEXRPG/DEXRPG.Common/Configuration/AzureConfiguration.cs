daa35b8d84f4df0d1c9f2e25a2857a98a121e04a
kaal87 <kaal1987@outlook.com>
2025-01-19 18:38:31 +0000

    added semi-colon

--- api/src/DEXRPG/DEXRPG.Common/Configuration/AzureConfiguration.cs
+++ api/src/DEXRPG/DEXRPG.Common/Configuration/AzureConfiguration.cs
@@ -11,7 +11,7 @@ public class AzureConfiguration
         new DefaultAzureCredential();
     #else 
     
-       new ManagedIdentityCredential("16575c00-1d5a-4ad2-8808-7a382f1ccf60")
+       new ManagedIdentityCredential("16575c00-1d5a-4ad2-8808-7a382f1ccf60");
     #endif
     
 }
