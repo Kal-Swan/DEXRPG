86ce147c694d8cf3de004d174abefbf677b08751
Kal-Swan <kal-swan@outlook.com>
2025-01-19 15:15:55 +0000

    added conditional azure config (#35)
    Co-authored-by: kaal87 <kaal1987@outlook.com>

---
+++ api/src/DEXRPG/DEXRPG.Common/Configuration/AzureConfiguration.cs
@@ -0,0 +1,24 @@
+using Azure.Core;
+using Azure.Identity;
+
+namespace DEXRPG.Common.Configuration;
+
+public class AzureConfiguration
+{
+    public TokenCredential Credential() =>
+
+#if DEBUG
+        new DefaultAzureCredential();
+    
+    #else 
+    
+       new DefaultAzureCredential( new DefaultAzureCredentialOptions
+       {
+           ExcludeSharedTokenCacheCredential = true,
+           ExcludeVisualStudioCredential = true,
+           ExcludeAzureCliCredential = true,
+           ExcludeManagedIdentityCredential = false
+       });
+    #endif
+    
+}
